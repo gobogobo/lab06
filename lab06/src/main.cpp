e2c4fb141a86841bb015b6c1c7951a0611d8b94e
gobogobo <32005527+gobogobo@users.noreply.github.com>
2017-10-11 21:47:00 -0700

    Initial commit

---
+++ lab06/src/main.cpp
@@ -0,0 +1,181 @@
+/* Lab 6 base code - transforms using local matrix functions
+	to be written by students -
+	based on lab 5 by CPE 471 Cal Poly Z. Wood + S. Sueda
+	& Ian Dunn, Christian Eckhardt
+*/
+#include <iostream>
+#include <glad/glad.h>
+
+#include "GLSL.h"
+#include "Program.h"
+#include "Shape.h"
+#include "MatrixStack.h"
+#include "WindowManager.h"
+
+// used for helper in perspective
+#include "glm/glm.hpp"
+// value_ptr for glm
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace std;
+using namespace glm;
+
+class Application : public EventCallbacks
+{
+
+public:
+
+	WindowManager * windowManager = nullptr;
+
+	// Our shader program
+	std::shared_ptr<Program> prog;
+
+	// Shape to be used (from obj file)
+	shared_ptr<Shape> shape;
+
+	// Contains vertex information for OpenGL
+	GLuint VertexArrayID;
+
+	// Data necessary to give our triangle to OpenGL
+	GLuint VertexBufferID;
+
+	void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods)
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+	}
+
+	void mouseCallback(GLFWwindow *window, int button, int action, int mods)
+	{
+		double posX, posY;
+
+		if (action == GLFW_PRESS)
+		{
+			glfwGetCursorPos(window, &posX, &posY);
+			cout << "Pos X " << posX <<  " Pos Y " << posY << endl;
+		}
+	}
+
+	void resizeCallback(GLFWwindow *window, int width, int height)
+	{
+		glViewport(0, 0, width, height);
+	}
+
+	void init(const std::string& resourceDirectory)
+	{
+		GLSL::checkVersion();
+
+		// Set background color.
+		glClearColor(0.12f, 0.34f, 0.56f, 1.0f);
+
+		// Enable z-buffer test.
+		glEnable(GL_DEPTH_TEST);
+
+		//culling:
+		glEnable(GL_CULL_FACE);
+		glFrontFace(GL_CCW);
+
+
+		// Initialize the GLSL program.
+		prog = make_shared<Program>();
+		prog->setVerbose(true);
+		prog->setShaderNames(resourceDirectory + "/simple_vert.glsl", resourceDirectory + "/simple_frag.glsl");
+		if (! prog->init())
+		{
+			std::cerr << "One or more shaders failed to compile... exiting!" << std::endl;
+			exit(1);
+		}
+		prog->init();
+		prog->addUniform("P");
+		prog->addUniform("MV");
+		prog->addAttribute("vertPos");
+		prog->addAttribute("vertNor");
+	}
+
+	void initGeom(const std::string& resourceDirectory)
+	{
+		// Initialize mesh.
+		shape = make_shared<Shape>();
+		shape->loadMesh(resourceDirectory + "/IcoSphere.obj");
+		shape->resize();
+		shape->init();
+		
+	}
+
+	void render()
+	{
+		// Get current frame buffer size.
+		int width, height;
+		glfwGetFramebufferSize(windowManager->getHandle(), &width, &height);
+		float aspect = width / (float)height;
+		glViewport(0, 0, width, height);
+
+		auto P = std::make_shared<MatrixStack>();
+		auto MV = std::make_shared<MatrixStack>();
+		P->pushMatrix();	
+		P->perspective(70., width, height, 0.1, 100.0f);
+
+		MV->pushMatrix();
+		MV->translate(glm::vec3(0,0,-3));
+
+		
+
+		// Clear framebuffer.
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		
+		// Draw mesh using GLSL
+		prog->bind();
+		
+		glUniformMatrix4fv(prog->getUniform("P"), 1, GL_FALSE, glm::value_ptr(P->topMatrix()));
+		glUniformMatrix4fv(prog->getUniform("MV"), 1, GL_FALSE, glm::value_ptr(MV->topMatrix()));
+
+		shape->draw(prog);
+		prog->unbind();
+	}
+};
+//*********************************************************************************************************
+int main(int argc, char **argv)
+{
+	// Where the resources are loaded from
+	std::string resourceDir = "../resources";
+
+	if (argc >= 2)
+	{
+		resourceDir = argv[1];
+	}
+
+	Application *application = new Application();
+
+	// Your main will always include a similar set up to establish your window
+	// and GL context, etc.
+
+	WindowManager *windowManager = new WindowManager();
+	windowManager->init(640, 480);
+	windowManager->setEventCallbacks(application);
+	application->windowManager = windowManager;
+
+	// This is the code that will likely change program to program as you
+	// may need to initialize or set up different data and state
+
+	application->init(resourceDir);
+	application->initGeom(resourceDir);
+
+	// Loop until the user closes the window.
+	while (! glfwWindowShouldClose(windowManager->getHandle()))
+	{
+		// Render scene.
+		application->render();
+
+		// Swap front and back buffers.
+		glfwSwapBuffers(windowManager->getHandle());
+		// Poll for and process events.
+		glfwPollEvents();
+	}
+
+	// Quit program.
+	windowManager->shutdown();
+	return 0;
+}
